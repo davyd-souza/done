6048958d5cde253c32fbcc990c5c79229c4a02ba
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2023-03-25 17:55:52 -0300

    feat: create header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+// Primary: 500, 400
+// Secondary: 400, 300
 export function App() {
   return (
-    <div className='App'>
-      <h1>Done</h1>
+    <div className='grid place-items-center gap-10'>
+      <header className='bg-neutral-200 dark:bg-neutral-800 w-full'>
+        <div className='mx-auto max-w-4xl'>
+          <h1 className='font-black text-primary-400 dark:text-secondary-200 text-3xl tracking-[1rem] flex justify-center p-8 select-none'>
+            DONE
+          </h1>
+        </div>
+      </header>
     </div>
   )
 }
